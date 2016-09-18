a714793ccbf908adc760b118d23d61cd25b0f809
Mason Brothers <brothers.mason@gmail.com>
2016-09-18 10:20:35 -0600

    Not working

--- app/src/main/java/net/migrapp/migraine/MainActivity.java
+++ app/src/main/java/net/migrapp/migraine/MainActivity.java
@@ -2,6 +2,8 @@ package net.migrapp.migraine;
 //Samsung API is licenced under Apache License
 
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.net.sip.SipSession;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.widget.TextView;
 
 import com.samsung.android.sdk.healthdata.HealthConnectionErrorResult;
 import com.samsung.android.sdk.healthdata.HealthConstants;
+import com.samsung.android.sdk.healthdata.HealthDataResolver;
 import com.samsung.android.sdk.healthdata.HealthDataService;
 import com.samsung.android.sdk.healthdata.HealthDataStore;
 import com.samsung.android.sdk.healthdata.HealthPermissionManager;
@@ -18,10 +21,11 @@ import com.samsung.android.sdk.healthdata.HealthResultHolder;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView helloWorld;
+    //private TextView helloWorld;
 
     private static final String APP_TAG = "NET_MIGRAP_MIGRAINE";
     private HealthDataService healthDataService;
@@ -68,21 +72,24 @@ public class MainActivity extends AppCompatActivity {
         }
         alert.show();
     }
-
+    /*
     private final HealthDataStore.ConnectionListener mConnectionListener = new HealthDataStore.ConnectionListener() {
         @Override
         public void onConnected() {
             Log.d(APP_TAG, "Health data service is connected.");
             HealthPermissionManager pmsManager = new HealthPermissionManager(mStore);
             try {
+                Log.i(APP_TAG, "About to setup listener");
                 // Check whether the permissions that this application needs are acquired
                 Map<HealthPermissionManager.PermissionKey, Boolean> resultMap = pmsManager.isPermissionAcquired(mKeySet);
                 if (resultMap.containsValue(Boolean.FALSE)) {
                     // Request the permission for reading step counts if it is not acquired
+                    Log.i(APP_TAG, "Setup Listener");
                     pmsManager.requestPermissions(mKeySet, MainActivity.this).setResultListener(mPermissionListener);
                 } else {
                     // Get the current step count and display it
                     // ...
+                    afterCall();
                 }
             } catch (Exception e) {
                 Log.e(APP_TAG, e.getClass().getName() + " - " + e.getMessage());
@@ -113,20 +120,94 @@ public class MainActivity extends AppCompatActivity {
 
                     } else {
                         // Get the current step count and display it
-                        helloWorld.setText(HealthConstants.Sleep.START_TIME);
-                        Log.i(APP_TAG, "MASON");
-                        // THE GOOD STUFF
-                        Log.i( APP_TAG, "CALDER WAS HERE" + HealthConstants.Sleep.START_TIME);
+                        afterCall();
                     }
                 }
             };
+
+    private final HealthResultHolder.ResultListener<HealthDataResolver.ReadResult> mListener = new HealthResultHolder.ResultListener<ReadResult>() {
+        @Override
+        public void onResult(HealthDataResolver.ReadResult result) {
+            int count = 0;
+            Cursor c = null;
+
+            try {
+                c = result.getResultCursor();
+                if (c != null) {
+                    while (c.moveToNext()) {
+                        count += c.getInt(c.getColumnIndex(HealthConstants.StepCount.COUNT));
+                    }
+                }
+            } finally {
+                if (c != null) {
+                    c.close();
+                }
+            }
+            MainActivity.getInstance().drawStepCount(String.valueOf(count));
+        }
+    };
+    */
+    private void afterCall() {
+        // helloWorld.setText(HealthConstants.Sleep.START_TIME);
+
+        HealthDataResolver resolver = new HealthDataResolver(mStore, null);
+
+        HealthDataResolver.Filter filter = filterIt(HealthConstants.CaffeineIntake.START_TIME);
+
+        //HealthResultHolder.ResultListener<HealthDataResolver.ReadResult> mListener = new HealthResultHolder.ResultListener<HealthDataResolver.ReadResult>();
+
+
+
+
+
+        HealthDataResolver.ReadRequest request = new HealthDataResolver.ReadRequest.Builder()
+                .setDataType(HealthConstants.StepCount.HEALTH_DATA_TYPE)
+                .setProperties(new String[] {HealthConstants.StepCount.COUNT})
+                .setFilter(filter)
+                .build();
+
+        try {
+            resolver.read(request).setResultListener(mListener);
+        } catch (Exception e) {
+            Log.e(MainActivity.APP_TAG, e.getClass().getName() + " - " + e.getMessage());
+            Log.e(MainActivity.APP_TAG, "Getting step count fails.");
+        }
+
+        Log.i( APP_TAG, "CALDER WAS HERE" + HealthConstants.WaterIntake.AMOUNT);
+    }
+
+
+
+    protected HealthDataResolver.Filter filterIt(String startTime) {
+        double currentTime = System.currentTimeMillis();
+        double yesterday = currentTime - 86400000;
+
+        return HealthDataResolver.Filter.greaterThan(
+                startTime, yesterday
+        );
+    }
+
+    protected HealthDataResolver.Filter filterIt(String startTime, String endTime) {
+        double currentTime = System.currentTimeMillis();
+        double yesterday = currentTime - 86400000;
+
+        return HealthDataResolver.Filter.and(
+                HealthDataResolver.Filter.greaterThan(
+                        startTime, yesterday
+                ),
+                HealthDataResolver.Filter.lessThan(
+                        endTime, currentTime
+                )
+        );
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        helloWorld = (TextView)findViewById(R.id.helloWorld);
+        //helloWorld = (TextView)findViewById(R.id.helloWorld);
 
         mKeySet = new HashSet<HealthPermissionManager.PermissionKey>();
         //Add Permissions
@@ -134,6 +215,8 @@ public class MainActivity extends AppCompatActivity {
         mKeySet.add(new HealthPermissionManager.PermissionKey(HealthConstants.OxygenSaturation.HEALTH_DATA_TYPE, HealthPermissionManager.PermissionType.READ));
         mKeySet.add(new HealthPermissionManager.PermissionKey(HealthConstants.Sleep.HEALTH_DATA_TYPE, HealthPermissionManager.PermissionType.READ));
         mKeySet.add(new HealthPermissionManager.PermissionKey(HealthConstants.WaterIntake.HEALTH_DATA_TYPE, HealthPermissionManager.PermissionType.READ));
+
+
 
         healthDataService = new HealthDataService();
         healthDataService.initialize(this);
